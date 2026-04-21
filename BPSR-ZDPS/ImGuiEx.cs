5dad858f3cae95151d49649dd30f18e39f71d980
ThiconZ <ThiconZ@users.noreply.github.com>
2026-04-21 01:53:31 -0400

    Added support for Circular duration progress bar style to Trackers in the Event Tracker. This style does not support having anything inside of it except for the icon. Added a new setting to display the Tracker Icon inside the progress bar. This is only supported by the Circle style mode currently.

--- BPSR-ZDPS/ImGuiEx.cs
+++ BPSR-ZDPS/ImGuiEx.cs
@@ -25,5 +25,52 @@ namespace BPSR_ZDPS
 
             ImGui.EndGroup();
         }
+
+        private static void DrawArc(float size, float max_angle_factor, float percentage, float thickness, Vector2 pos, ImTextureRef? imTextureRef = null)
+        {
+            var draw_list = ImGui.GetWindowDrawList();
+            
+            float x = pos.X;
+            float y = pos.Y;
+
+            const float ONE_DIV_360f = 1.0f / 360.0f;
+
+            float a_min_factor;
+            float a_max_factor;
+            float a_max_factor_100percentage;
+            a_min_factor = -1.5f + ((360 - max_angle_factor) * ONE_DIV_360f);
+            a_max_factor_100percentage = (a_min_factor + 1.0f) * -1.0f;
+
+            float a_factor_delta = (a_max_factor_100percentage - a_min_factor) * (percentage * 0.01f);
+            a_max_factor = a_min_factor + a_factor_delta;
+
+            if (imTextureRef != null)
+            {
+                draw_list.AddImageRounded(imTextureRef.Value, pos + new Vector2(8, 0), pos + new Vector2(size) + new Vector2(-6, 0), new Vector2(0, 0), new Vector2(1, 1), ImGui.GetColorU32(Colors.White), size * 0.5f, ImDrawFlags.RoundCornersAll);
+            }
+
+            // Path for background arc (dimmed arc)
+            draw_list.PathArcTo(new Vector2(x + size * 0.5f, y + size * 0.5f), size * 0.5f - thickness * 0.5f, 3.141592f * a_min_factor, 3.141592f * a_max_factor_100percentage);
+            draw_list.PathStroke(ImGui.GetColorU32(ImGuiCol.FrameBg), ImDrawFlags.None, thickness);
+            
+            // Path for progress filling (highlighted arc)
+            draw_list.PathArcTo(new Vector2(x + size * 0.5f, y + size * 0.5f), size * 0.5f - thickness * 0.5f, 3.141592f * a_min_factor, 3.141592f * a_max_factor);
+            draw_list.PathStroke(ImGui.GetColorU32(ImGuiCol.PlotHistogram), ImDrawFlags.None, thickness);
+        }
+
+        public static void ProgressBarArc(float size, float max_angle_factor, float percentage, Vector2 pos, float thickness, ImTextureRef? imTextureRef = null)
+        {
+            var windowPos = ImGui.GetWindowPos();
+
+            DrawArc(size, max_angle_factor, percentage, thickness, new Vector2(pos.X + windowPos.X, pos.Y + windowPos.Y), imTextureRef);
+        }
+
+        public static void ProgressBarArc(float size, float max_angle_factor, float percentage, float thickness = 2.0f, ImTextureRef? imTextureRef = null)
+        {
+            var pos = ImGui.GetCursorScreenPos();
+            ImGui.Dummy(new Vector2(size));
+
+            DrawArc(size, max_angle_factor, percentage, thickness, pos, imTextureRef);
+        }
     }
 }
